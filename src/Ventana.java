b27527093c7ee9c90fed63b8f5f43030102aed4a
KevinRP1702 <159939479+KevinRP1702@users.noreply.github.com>
2024-02-29 13:13:32 -0700

    Unidad 2: ejercicio 27

---
+++ src/Ventana.java
@@ -0,0 +1,175 @@
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GridLayout;
+
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.ButtonGroup;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JRadioButton;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+public class Ventana extends JFrame{
+	
+	public Ventana() {
+
+		this.setSize(520,720);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLocation(200,200);
+		this.setResizable(true);
+		this.setTitle("Calculando el interes");
+		this.setMinimumSize(new Dimension(250,250));
+		this.setMaximumSize(new Dimension(1000,750));
+		this.setLocationRelativeTo(null);
+		this.iniciarComponentes();
+		this.setLayout(null);
+		this.setVisible(true);
+	}
+	
+	//Metodo iniciar los componentes del panel
+	public void iniciarComponentes() {
+		
+		this.calculoInteres();
+;		this.repaint();
+	}
+	
+	public void calculoInteres() {
+		
+		//Panel del fondo del frame
+		JPanel pnlPrincipal = new JPanel();
+		pnlPrincipal.setSize(this.getWidth(),this.getHeight());
+		pnlPrincipal.setBackground(Color.white);
+		pnlPrincipal.setLayout( new BorderLayout());
+		
+		//Panel donde se ingresan los datos
+		JLabel pnlDatos = new JLabel();
+		pnlDatos.setOpaque(true);
+		pnlDatos.setPreferredSize(new Dimension(200,300));
+		pnlDatos.setBackground(Color.GREEN);
+		pnlDatos.setFont(new Font ("calibri",Font.BOLD,30));
+		pnlDatos.setBorder(BorderFactory.createLineBorder(Color.white,35));
+		pnlPrincipal.add(pnlDatos, BorderLayout.CENTER);
+		
+		
+		//Label del titulo
+		JLabel lblInteres = new JLabel("Calcular interés: ");
+		lblInteres.setBounds(50,25,180,40);
+		lblInteres.setFont(new Font ("Arial", Font.BOLD, 15));
+		pnlDatos.add(lblInteres);
+		
+		//Label de la capital
+		JLabel lblCapital = new JLabel("Capital: ");
+		lblCapital.setBounds(50,80,180,40);
+		lblCapital.setFont(new Font ("Agency FB", Font.BOLD, 20));
+		lblCapital.setForeground(Color.black);
+		pnlDatos.add(lblCapital);
+		
+		//Texto para ingresar capital
+		JTextField txtCapital = new JTextField();
+		txtCapital.setBounds(170,90,200,25);
+		txtCapital.setOpaque(true);
+		txtCapital.setBackground(Color.white);
+		pnlDatos.add(txtCapital);
+		
+		//Label para el tiempo
+		JLabel lblTiempo = new JLabel("Tiempo: ");
+		lblTiempo.setBounds(50,140,180,40);
+		lblTiempo.setFont(new Font ("Agency FB", Font.BOLD, 20));
+		lblTiempo.setForeground(Color.black);
+		pnlDatos.add(lblTiempo);
+		
+		//TypeTime Field
+		JTextField txtTiempo = new JTextField();
+		txtTiempo.setBounds(170,150,200,25);
+		txtTiempo.setOpaque(true);
+		txtTiempo.setBackground(Color.white);
+		pnlDatos.add(txtTiempo);
+		
+		//Label para la tasa de interes
+		JLabel txtTasa = new JLabel("Tasa de interés: ");
+		txtTasa.setBounds(50,200,180,40);
+		txtTasa.setFont(new Font ("Agency FB", Font.BOLD, 20));
+		txtTasa.setForeground(Color.black);
+		pnlDatos.add(txtTasa);
+		
+		//Texto para interes
+		JTextField txtInteres = new JTextField();
+		txtInteres.setBounds(170,210,200,25);
+		txtInteres.setOpaque(true);
+		txtInteres.setBackground(Color.white);
+		pnlDatos.add(txtInteres);
+		
+		//Label para el interes
+		JLabel interestText = new JLabel("Interés",2);
+		interestText.setFont(new Font ("calibri",Font.BOLD,30));
+		interestText.setForeground(Color.red);
+		pnlPrincipal.add(interestText, BorderLayout.NORTH);
+		
+		//boton para cancelar
+		JButton btnCalcular = new JButton("Calcular");
+		btnCalcular.setBounds(100, 300, 150, 40);
+		pnlDatos.add(btnCalcular);
+		
+		//Boton para cancelar
+		JButton btnCancelar = new JButton("Cancelar");
+		btnCancelar.setOpaque(true);
+		btnCancelar.setBounds(280, 300, 150, 40);
+		pnlDatos.add(btnCancelar);
+		
+		
+		//Panel donde se muestra la informacion
+		JLabel pnlResultados = new JLabel();
+		pnlResultados.setOpaque(true);
+		pnlResultados.setPreferredSize(new Dimension(200,300));
+		pnlResultados.setBackground(Color.PINK);
+		pnlResultados.setBorder(BorderFactory.createLineBorder(Color.white,35));
+		pnlPrincipal.add(pnlResultados, BorderLayout.SOUTH);
+				
+		//Label del interes 
+		JLabel interest = new JLabel("Interés: ");
+		interest.setBounds(50,80,180,40);
+		interest.setFont(new Font ("Agency FB", Font.BOLD, 20));
+		interest.setForeground(Color.black);
+		pnlResultados.add(interest);
+		
+		//Texto ingresar interes 
+		JTextField txtInteres2 = new JTextField();
+		txtInteres2.setBounds(170,90,200,25);
+		txtInteres2.setOpaque(true);
+		txtInteres2.setBackground(Color.white);
+		pnlResultados.add(txtInteres2);
+		
+		//Label para el monto
+		JLabel lblMonto = new JLabel("Monto: ");
+		lblMonto.setBounds(50,140,180,40);
+		lblMonto.setFont(new Font ("Agency FB", Font.BOLD, 20));
+		lblMonto.setForeground(Color.black);
+		pnlResultados.add(lblMonto);
+		
+		//Texto del monto
+		JTextField txtMonto = new JTextField();
+		txtMonto.setBounds(170,150,200,25);
+		txtMonto.setOpaque(true);
+		txtMonto.setBackground(Color.white);
+		pnlResultados.add(txtMonto);
+		
+		this.add(pnlPrincipal);
+	}
+}
